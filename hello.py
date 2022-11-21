7088e4d5061e3f8c4f073e8895bdf10520091575
sinyoung2 <105351819+kimsinyoung2@users.noreply.github.com>
2022-11-21 11:07:01 +0900

    retest python commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print('hello')
+print("hello")
+a = 10
+b = 20
+print(a+b)
+inputA = int(input("number1 :"))
+inputB = int(input("number2 :"))
+# python 3.x f-string
+print(f"{inputA} + {inputB} = {inputA+inputB}")
+# python 1~10까지 출력
+for i in range(1,11):
+    print(i)
